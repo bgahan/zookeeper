7cb699e72c6aefd7ba7ef906520ceb3905b9685d
Brittany Gahan <brittanygahan89@gmail.com>
2022-05-30 15:09:28 -0400

    create get apis

--- server.js
+++ server.js
@@ -46,6 +46,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 };
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 // create the API/route to get animal data
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -55,6 +60,16 @@ app.get('/api/animals', (req, res) => {
     }
 
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 // tell the server to listen for requests
